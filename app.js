b1a6afee50f8d44cf6ca6512b4704aaf541bf47c
thrashraf <zulashrafvns@gmail.com>
2021-03-04 22:42:41 +0800

    hehe

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express')
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static('public'));
+
+app.get('/' , (req , res)=> {
+
+    res.render('home')
+})
+
+
+app.listen(3000 , (req , res )=> {
+
+    console.log('server start at port 3000')
+
+
+})
---
+++ public/inde.js
@@ -0,0 +1,6 @@
+const btn =  document.getElementById("#darkmode"); 
+        
+btn.addEventListener("click", function() {
+    // document.body.classList.toggle("dark-theme");
+    alert('CLICK')
+ })
